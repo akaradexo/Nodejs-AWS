c2caa92250c001a16b407a6a2fa18d4cf49bd18a
Ashutosh Karade <69238714+akaradexo@users.noreply.github.com>
2022-08-13 12:45:01 +0530

    sum of integers ex 8

---
+++ node assignment/ex8.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+// var fileName = process.argv.slice(2);
+
+// // syschronous
+
+var readMe= fs.readFile("num.txt",'utf8' ,function(err,data){
+    if(err) console.log('error', err);
+    var numArr = data.split(" ")
+    console.log(numArr)
+    var sum =  0
+    numArr.forEach(x =>{
+        var y = parseInt(x)
+        console.log(y)
+        sum+=y;
+    })
+    console.log(sum) ;
+});
+console.log(readMe);
