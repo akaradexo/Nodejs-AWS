0e190ec0718cb564aec954b386702c69f592b0b4
Ashutosh Karade <69238714+akaradexo@users.noreply.github.com>
2022-08-13 10:26:51 +0530

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,9 @@
+var http = require ('http');
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/plain'});
+    res.end('i m listening to the port : 3000');
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('i m listening to the port : 3000')
