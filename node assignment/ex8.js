001781b341541025cead5abef21afbe6505a21fe
Ashutosh Karade <69238714+akaradexo@users.noreply.github.com>
2022-08-13 12:52:03 +0530

    update

--- node assignment/ex8.js
+++ node assignment/ex8.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var readMe= fs.readFile("num.txt",'utf8' ,function(err,data){
     if(err) console.log('error', err);
-    var numArr = data.split(" ")
+    var numArr = data.split('\n')
     console.log(numArr)
     var sum =  0
     numArr.forEach(x =>{
